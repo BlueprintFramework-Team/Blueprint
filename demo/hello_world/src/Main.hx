0cff392cfb4207f5881e5056f90fe248fe069e55
MidnightBloxxer <138817584+MidnightBloxxer@users.noreply.github.com>
2023-08-13 16:48:20 -0400

    Fix hello_world demo with api update

--- demo/hello_world/src/Main.hx
+++ demo/hello_world/src/Main.hx
@@ -7,6 +7,6 @@ class Main {
 
 	public static function main():Void {
 		// Width, Height, Scene (Class, not Scene instance)
-		game = new Game(640, 480, HelloWorld);
+		game = new Game(640, 480, 'HelloWorld', HelloWorld);
 	}
 }
