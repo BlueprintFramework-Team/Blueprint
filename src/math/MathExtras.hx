f83754478e006ee1ca21149a95cd5c26c65338af
MidnightBloxxer <138817584+MidnightBloxxer@users.noreply.github.com>
2023-10-24 21:56:05 -0400

    Added MathExtras.lerp(a,b,r)

--- src/math/MathExtras.hx
+++ src/math/MathExtras.hx
@@ -1,11 +1,15 @@
 package math;
 
 class MathExtras {
-    public static inline function toRad(deg:Float) {
-        return deg * Math.PI / 180;
-    }
+	public static inline function toRad(deg:Float) {
+		return deg * Math.PI / 180;
+	}
 
-    public static inline function toDeg(rad:Float) {
-        return rad * 180 / Math.PI;
-    }
+	public static inline function toDeg(rad:Float) {
+		return rad * 180 / Math.PI;
+	}
+
+	public static inline function lerp(a:Float, b:Float, r:Float):Float {
+		return a + r * (b - a);
+	}
 }
