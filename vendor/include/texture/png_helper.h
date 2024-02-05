3979f6d9f92e6692a23d83fa814f24bac0cb574d
SrtHero278 <srtpro278@gmail.com>
2024-02-05 07:50:45 -0800

    Using row_stride speeds it up a bit!
    Not a lot but every bit counts!

--- vendor/include/texture/png_helper.h
+++ vendor/include/texture/png_helper.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#ifndef INCLUDED_Sys
-#include <Sys.h>
-#endif
-
 #include <includeWorkaround.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,11 +12,12 @@ int loadPng(const char* path, int* width, int* height) {
 
     if (png_image_begin_read_from_file(&img, path) != 0) {
         img.format = PNG_FORMAT_RGBA;
-        png_bytep buffer = (png_bytep)malloc(PNG_IMAGE_SIZE(img));
+        png_uint_32 row_stride = PNG_IMAGE_ROW_STRIDE(img);
+        png_bytep buffer = (png_bytep)malloc(PNG_IMAGE_BUFFER_SIZE(img, row_stride));
 
-        if (png_image_finish_read(&img, NULL, buffer, 0, NULL) != 0) {
-            *width = (int)img.width;
-            *height = (int)img.height;
+        if (png_image_finish_read(&img, NULL, buffer, row_stride, NULL) != 0) {
+            *width = img.width;
+            *height = img.height;
 
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.width, img.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
             png_image_free(&img);
